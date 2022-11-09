802ccd232bad05b7936043f7f1ec894338c27481
Mehtap Tataroğlu <teytap@gmail.com>
2022-11-09 19:18:16 +0300

    added git-hub source

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <header>hello</header>
+      <footer>
+        This project is coded bay Mehtap Tataroğlu and is{" "}
+        <a href="https://github.com/teytap/me-weather-react" target="_blank">
+          open-sourced
+        </a>
+        .
+      </footer>
     </div>
   );
 }
