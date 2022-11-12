4b00b05e980f94976dca491a8d5530a09608e377
Mehtap Tataroğlu <teytap@gmail.com>
2022-11-12 13:17:22 +0300

    added WeatherTemperature component to change units

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./WeatherTemperature.css";
 export default function WeatherTemperature(props) {
-  return (
-    <span className="WeatherTemperature">
-      <span className="temperature">{props.celcius}</span>
-      <span className="unit">°C</span>
-    </span>
-  );
+  let [unit, setUnit] = useState("celcius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelcius(event) {
+    event.preventDefault();
+    setUnit("celcius");
+  }
+  if (unit === "celcius") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celcius)}</span>
+        <span className="unit">
+          <span className="celcius">°C</span> |
+          <a href="#" className="fahrenheit" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <span className="temperature">
+          {Math.round((props.celcius * 9) / 5 + 32)}
+        </span>
+        <span className="unit">
+          <a href="#" className="celcius" onClick={showCelcius}>
+            °C
+          </a>{" "}
+          |<span className="fahrenheit">°F</span>
+        </span>
+      </span>
+    );
+  }
 }
